69773413a04ecc762c1d57c5d856c2911f37f410
alamgirmani <alamgir.mani@gmail.com>
2021-08-11 13:10:54 +0200

    added multiple python files

---
+++ sandbox_request/api.py
@@ -0,0 +1,28 @@
+"""
+    Module aoi.py
+"""
+# Copyright 2021 Universität Tübingen, DKFZ and EMBL
+# for the German Human Genome-Phenome Archive (GHGA)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from fastapi import FastAPI
+
+from sandbox_request.database import get_db, close_db
+from sandbox_request.routes.requests import request_router
+
+app = FastAPI(title="Request Service API")
+
+app.include_router(request_router)
+app.add_event_handler("startup", get_db)
+app.add_event_handler("shutdown", close_db)
